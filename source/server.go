2e2bcab26a4d5b2bf88888f4647b7b3f2a5ce746
Laik4 <qx.oco.xp@gmail.com>
2019-08-26 21:33:47 +0900

    update submission

--- source/server.go
+++ source/server.go
@@ -2,54 +2,56 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
-	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"github.com/prologic/bitcask"
 )
 
 // EnrollFlag enrolls the flag
-func EnrollFlag(id int, flag string) {
-	bid := []byte(strconv.Itoa(id))
+func EnrollFlag(id string, flag string) error {
+	bid := []byte(id)
 	bflag := []byte(flag)
 	db, err := bitcask.Open("databases/flag")
 	defer db.Close()
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "[-] %v\n", err)
-		return
+		return err
 	}
-	db.Put(bid, bflag)
+	if !db.Has(bid) {
+		db.Put(bid, bflag)
+	} else {
+		fmt.Fprintln(os.Stderr, "Another flag has been already enrolled")
+		return fmt.Errorf("Another flag has been already enrolled")
+	}
 	fmt.Printf("[+] Enroll { %v : %v }\n", id, flag)
+	return nil
 }
 
 // GetFlag gets the flag
-func GetFlag(id int) string {
-	bid := []byte(strconv.Itoa(id))
+func GetFlag(id string) (string, error) {
+	bid := []byte(id)
 	db, _ := bitcask.Open("databases/flag")
 	defer db.Close()
-	flag := ""
 	bflag, err := db.Get(bid)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "[-] %v\n", err)
-		return ""
+		return "", err
 	}
-	flag = string(bflag)
-	return flag
+	return string(bflag), nil
 }
 
 // DeleteFlag deletes the flag
-func DeleteFlag(id int) {
-	bid := []byte(strconv.Itoa(id))
+func DeleteFlag(id string) error {
+	bid := []byte(id)
 	db, _ := bitcask.Open("databases/flag")
 	defer db.Close()
 	flag, err := db.Get(bid)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "[-] %v\n", err)
-		return
+		return err
 	}
 	db.Delete(bid)
 	fmt.Printf("[+] Delete { %v : %v }\n", id, string(flag))
+	return nil
 }
 
 // CheckFlag checks whether the submitted flag is correct or not
@@ -62,32 +64,55 @@ func CheckFlag(id string, submission string) (bool, error) {
 		fmt.Fprintf(os.Stderr, "[-] %v\n", err)
 		return false, err
 	}
-
 	return string(bflag) == submission, nil
-
 }
 
 func main() {
 	fmt.Print("[+] Starting server...\n")
 	r := gin.Default()
 	r.LoadHTMLGlob("templates/*.html")
-	EnrollFlag(0, "FLAG")
-	// flag := GetFlag(0)
-	// fmt.Println(flag)
-	// flag2 := GetFlag(5)
-	// fmt.Println(flag2)
-	// DeleteFlag(0)
-	r.POST("/submit", func(c *gin.Context) {
+
+	EnrollFlag("0", "FLAG")
+
+	r.PUT("/", func(c *gin.Context) {
+		c.Request.ParseForm()
+		problemid := c.Request.Form["id"]
+		flag := c.Request.Form["flag"]
+		err := EnrollFlag(problemid[0], flag[0])
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "[-] %v\n", err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+		} else {
+			c.JSON(http.StatusOK, gin.H{"error": err})
+		}
+
+	})
+	r.POST("/", func(c *gin.Context) {
 		c.Request.ParseForm()
 		problemid := c.Request.Form["id"]
 		submittedflag := c.Request.Form["flag"]
 		correct, err := CheckFlag(problemid[0], submittedflag[0])
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "[-] %v\n", err)
-
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err, "correct": false})
+		} else {
+			c.JSON(http.StatusOK, gin.H{"error": err, "correct": correct})
 		}
-		c.HTML(200, "index.html", gin.H{"flag": submittedflag, "correct": correct})
 
+	})
+	r.DELETE("/", func(c *gin.Context) {
+		c.Request.ParseForm()
+		problemid := c.Request.Form["id"]
+		for key, value := range c.Request.Form {
+			fmt.Fprintf(os.Stderr, "%v: %v\n", key, value)
+		}
+		err := DeleteFlag(problemid[0])
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "[-] %v\n", err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+		} else {
+			c.JSON(http.StatusOK, gin.H{"error": err})
+		}
 	})
 	r.Run()
 }
